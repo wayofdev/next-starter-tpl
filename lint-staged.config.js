e8ec6c657ef2d2a92b0006560b4b2498a1f9d964
lotyp <lotyp7@gmail.com>
2022-12-16 14:15:17 +0200

    feat: connecting linters

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,19 +1,19 @@
-const config = require("@wayofdev/lint-staged-config");
-const ymlConfig = require("@wayofdev/lint-staged-config/yml");
+const jsConfig = require('@wayofdev/lint-staged-config')
+const ymlConfig = require('@wayofdev/lint-staged-config/yml')
 
 module.exports = {
     // check for credentials
-    "*": ["secretlint"],
+    '*': ['secretlint'],
     // ignore prettier on unknown extensions
-    "!(*.{md,js,jsx,ts,tsx,json,css,scss,yml,yaml})": [
-        "prettier --cache --write --ignore-unknown",
+    '!(*.{md,js,jsx,ts,tsx,json,css,scss,yml,yaml})': [
+        'prettier --cache --write --ignore-unknown',
     ],
     ...ymlConfig,
-    ...config,
+    ...jsConfig,
     // lint and fix changed markdown files
-    "*.md": ["prettier --cache --write", "markdownlint"],
+    '*.md': ['prettier --cache --write', 'markdownlint'],
     // lint and fix changed json files
-    "*.json": ["prettier --cache --write"],
+    '*.json': ['prettier --cache --write'],
     // lint and fix changed css and scss files
-    "*.{css,scss}": ["prettier --cache --write", "stylelint --cache --fix"],
-};
+    '*.{css,scss}': ['prettier --cache --write', 'stylelint --cache --fix'],
+}
