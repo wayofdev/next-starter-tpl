9dc21e5e02bde07bb830505ac8371d1b1ce5391f
lotyp <lotyp7@gmail.com>
2022-12-22 16:36:24 +0200

    feat: add nextra docs template

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  reactStrictMode: true,
-  experimental: {
-    transpilePackages: ["ui"],
-  },
-};
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
+
+module.exports = withNextra()
---
+++ apps/docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Default Project</span>,
+  project: {
+    link: 'https://github.com/wayofdev/next-starter-tpl',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/wayofdev/next-starter-tpl',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
