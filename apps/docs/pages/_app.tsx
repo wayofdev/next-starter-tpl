61cf7cad875d871be995a5b50f19c51d790a06ac
lotyp <lotyp7@gmail.com>
2023-01-10 20:36:37 +0200

    feat: docs, custom styles

---
+++ apps/docs/pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from 'next/app'
+import type { ReactElement } from 'react'
+
+import '../style.css'
+
+export default function Nextra({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />
+}
