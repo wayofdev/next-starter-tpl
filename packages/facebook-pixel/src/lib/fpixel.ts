d8aa58e5bcbd3fcc469eaac3398bcc0b52adf805
lotyp <lotyp7@gmail.com>
2023-02-03 15:07:40 +0200

    fix: facebook-pixel types missing

--- packages/facebook-pixel/src/lib/fpixel.ts
+++ packages/facebook-pixel/src/lib/fpixel.ts
@@ -1,5 +1,11 @@
 export const facebookPixelId = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID
 
+declare global {
+  interface Window {
+    fbq: facebook.Pixel.Event
+  }
+}
+
 // https://developers.facebook.com/docs/facebook-pixel/advanced/
 export const event = (name: string, options = {}) => {
   window.fbq('track', name, options)
