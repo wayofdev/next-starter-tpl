5711c7a2e46a813a3a05390dc8a1820acb70734d
lotyp <lotyp7@gmail.com>
2023-02-22 22:07:28 +0200

    fix: lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,21 +2,14 @@
 
 const { concatFilesForPrettier } = require('@wayofdev/lint-staged-config')
 
-const { json } = require('@wayofdev/lint-staged-config/bases')
-const { yaml } = require('@wayofdev/lint-staged-config/bases')
-const { secrets } = require('@wayofdev/lint-staged-config/bases')
-const { md } = require('@wayofdev/lint-staged-config/bases')
-
 const rules = {
-  ...json,
-  ...yaml,
-  ...secrets,
-  ...md,
+  ...require('@wayofdev/lint-staged-config/json'),
+  ...require('@wayofdev/lint-staged-config/yaml'),
+  ...require('@wayofdev/lint-staged-config/secrets'),
+  ...require('@wayofdev/lint-staged-config/md'),
   '**/*.{js,jsx,cjs,mjs,ts,tsx,mts,cts}': filenames => {
     return [`prettier --write ${concatFilesForPrettier(filenames)}`]
   },
 }
 
-console.log(rules);
-
-// module.exports = rules
+module.exports = rules
