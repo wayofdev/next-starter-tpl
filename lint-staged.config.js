be5654063468d213f92439d3423730823fecbe21
lotyp <lotyp7@gmail.com>
2023-02-22 19:35:57 +0200

    docs: refactoring

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,10 +3,10 @@
 const { concatFilesForPrettier } = require('@wayofdev/lint-staged-config')
 
 
-const jsonRules = require('@wayofdev/lint-staged-config/json')
-const secretsRules = require('@wayofdev/lint-staged-config/secrets')
-const mdRules = require('@wayofdev/lint-staged-config/md')
-const yamlRules = require('@wayofdev/lint-staged-config/yaml')
+const { jsonRules } = require('@wayofdev/lint-staged-config/json')
+const { secretsRules } = require('@wayofdev/lint-staged-config/secrets')
+const { mdRules } = require('@wayofdev/lint-staged-config/md')
+const { yamlRules } = require('@wayofdev/lint-staged-config/yaml')
 
 console.log(...yamlRules);
 
