86f3e92a86f2b8ec7d9c23d3a954da3cb13a6afd
lotyp <lotyp7@gmail.com>
2023-02-22 19:37:52 +0200

    chore: clean config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,10 @@
 // @ts-check
 
 const { concatFilesForPrettier } = require('@wayofdev/lint-staged-config')
-
 const { jsonRules } = require('@wayofdev/lint-staged-config/json')
 const { secretsRules } = require('@wayofdev/lint-staged-config/secrets')
 const { mdRules } = require('@wayofdev/lint-staged-config/md')
 const { yamlRules } = require('@wayofdev/lint-staged-config/yaml')
-
-console.log(mdRules)
 
 const rules = {
   ...jsonRules,
