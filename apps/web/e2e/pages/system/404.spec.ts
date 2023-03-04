3554b49c7ad93884f30de7d63f82010f2151a05a
murashkin.i <murashkin.i@my.com>
2023-03-04 14:00:22 +0200

    chore: fix fbq event while tests

--- apps/web/e2e/pages/system/404.spec.ts
+++ apps/web/e2e/pages/system/404.spec.ts
@@ -6,12 +6,12 @@ const pageSlug = 'this-page-does-not-exist'
 
 test.describe('404 not found page', () => {
   test('should have the title in english by default', async ({ page }) => {
-    await page.goto(`http://localhost:3000/${pageSlug}`)
+    await page.goto(`/${pageSlug}`)
     const title = await page.title()
     expect(title).toBe(systemJsonEn.notFound.title)
   })
   test('should have the title in french', async ({ page }) => {
-    await page.goto(`http://localhost:3000/fr/${pageSlug}`)
+    await page.goto(`/fr/${pageSlug}`)
     const title = await page.title()
     expect(title).toBe(systemJsonFr.notFound.title)
   })
