71871bd6bc9e793a084106216d6d40874cd02efd
lotyp <lotyp7@gmail.com>
2023-03-05 13:55:36 +0200

    feat: add index page test

---
+++ apps/web/e2e/pages/index/index.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test'
+import homeJsonEn from '@wayofdev/common-i18n/locales/en/demo.json'
+import homeJsonFr from '@wayofdev/common-i18n/locales/fr/demo.json'
+
+test.describe('Demo page', () => {
+  test('should have the title in english by default', async ({ page }) => {
+    await page.goto('/')
+    const title = await page.title()
+    expect(title).toBe(homeJsonEn.page.title)
+  })
+
+  test('should have the title in french', async ({ page }) => {
+    await page.goto('/fr')
+    const title = await page.title()
+    expect(title).toBe(homeJsonFr.page.title)
+  })
+})
