110198167a80938a9e2dfb3af7ecc9f5b8a290f8
murashkin.i <murashkin.i@my.com>
2023-03-12 14:35:24 +0200

    test: adding Dropdown tests

--- packages/ui/src/base/button/__tests__/Button.test.tsx
+++ packages/ui/src/base/button/__tests__/Button.test.tsx
@@ -2,21 +2,14 @@ import renderer from 'react-test-renderer'
 import Button, { Size, Mode } from '../Button'
 
 describe('Button', () => {
-  const sizes = Object.values(Size)
-  const modes = Object.values(Mode)
-
-  sizes.forEach(size => {
-    modes.forEach(mode => {
-      const indexOfSize = Object.values(Size).indexOf(size as unknown as Size)
-      const indexOfMode = Object.values(Mode).indexOf(mode as unknown as Mode)
-
-      const keyOfSize = Object.keys(Size)[indexOfSize]
-      const keyOfMode = Object.keys(Mode)[indexOfMode]
-
-      it(`renders correctly with size=${keyOfSize} and mode=${keyOfMode}`, () => {
-        const tree = renderer.create(<Button mode={mode} size={size} label="Button" />).toJSON()
+  for (const [sizeKey, sizeValue] of Object.entries(Size)) {
+    for (const [modeKey, modeValue] of Object.entries(Mode)) {
+      it(`renders correctly with size=${sizeKey} and mode=${modeKey}`, () => {
+        const tree = renderer
+          .create(<Button mode={modeValue} size={sizeValue} label="Button" />)
+          .toJSON()
         expect(tree).toMatchSnapshot()
       })
-    })
-  })
+    }
+  }
 })
